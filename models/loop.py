dd3fb9926d9a3ea0a517de313db6999bb37f83bb
NaroaCS <naroa.coretti@gmail.com>
2023-06-20 14:54:32 -0400

    code for restarting Gama

--- models/loop.py
+++ models/loop.py
@@ -2,6 +2,7 @@ import pyautogui
 import time
 import pygetwindow as gw
 import schedule
+import win32gui
 
 #Find the right window name
 if False:
@@ -12,24 +13,36 @@ if False:
     for window in windows:
         print(window)
 
+#Change title
+if False:
+    #Avoid backlash command with r
+    window_title= r'test - C:\Users\Naroa\Documents\GitHub\FoodDeliveries\models\TestPlot.gaml'
+    hwnd = win32gui.FindWindow(None, window_title)
+
+    new_title = 'GAMA'
+    win32gui.SetWindowText(hwnd, new_title)
 
 
-def restart_gama():
-    #write
-    print('Hello')
+if True:
+    def restart_gama():
 
-    # Find the GAMA window by its title
-    gama_window = gw.getWindowWithTitle('Gama')[0]
-    gama_window.activate()
+        # Find the GAMA window by its title
+        gama_windows = gw.getWindowsWithTitle('GAMA')
 
-    # Simulate pressing the Control + R keyboard shortcut
-    pyautogui.hotkey('command', 'r')
-    time.sleep(5)  # Wait for GAMA to restart (adjust if needed)
+        if len(gama_windows) == 0:
+            print('GAMA window not found')
+            return
 
-# Schedule the restart every hour
-schedule.every().minute.do(restart_gama)
+        gama_windows[0].activate()
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+        # Simulate pressing the Control + R keyboard shortcut
+        pyautogui.hotkey('ctrl', 'r')
+        time.sleep(5)  # Wait for GAMA to restart (adjust if needed)
+
+    # Schedule the restart every hour
+    schedule.every(10).seconds.do(restart_gama)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
